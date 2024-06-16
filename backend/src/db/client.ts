af58f6b8893cb52c0c4202fc325ba695dc92c26e
Hernan Diaz <hernandiaz035@gmail.com>
2024-06-16 18:32:16 +1000

    correct

--- backend/src/db/client.ts
+++ backend/src/db/client.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
+import { usersTable } from "./schemas/users";
 
-const sqlite = new Database("sqlite.db");
+const sqlite = new Database("db.sqlite");
 
-export const db = drizzle(sqlite);
+export const db = drizzle(sqlite, { schema: { usersTable } });
